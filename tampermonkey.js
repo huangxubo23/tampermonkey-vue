9d3af42e6bb933aa3656e2f7abd5765c8c89c9f1
Harry Huang <732150595@qq.com>
2020-11-20 10:58:59 +0800

    feat: init code

---
+++ tampermonkey.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         __APP_NAME__
+// @namespace    http://tampermonkey.net/
+// @version      __APP_VERSION__
+// @description  使用Vue2.x element-ui构建油猴TamperMonkey插件
+// @author       harry
+// @run-at       document-start
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_setClipboard
+// @grant        GM_info
+// @grant        GM_xmlhttpRequest
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.setClipboard
+// @grant        GM_info
+// @grant        GM.xmlHttpRequest
+// @require      https://cdn.bootcdn.net/ajax/libs/vue/2.6.12/vue.min.js
+// @require      https://unpkg.com/element-ui@2.14.1/lib/index.js
+// @require      https://cdn.bootcdn.net/ajax/libs/axios/0.21.0/axios.min.js
+// @include      *
+// ==/UserScript==
